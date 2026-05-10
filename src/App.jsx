c3b7c4bf14cbe0934a14a864127e82fb53c86445
Nusrat1002 <nusrat.j.jannat103@gmail.com>
2026-05-10 22:16:44 +0600

    created Form.jsx and moved the form to that

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,7 @@
-import { useState } from "react";
-
 const App = () => {
-  const [form, setForm] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    phone: "",
-  });
-
-  const handleChange =(e)=>{
-    const {name, value} =e.target;
-    setForm({ ...form, [name]: value});
-  };
-
   return (
-    <form action="" className="flex flex-col gap-4 m-5">
-      <input
-        className="border-2 border-black p-5"
-        placeholder="First Name"
-        type="text"
-        name="firstName"
-        onChange={(e)=> handleChange(e)}
-        value={form.firstName}
-      />
-      <input
-        className="border-2 border-black p-5"
-        placeholder="Last Name"
-        type="text"
-        name="lastName"
-        onChange={(e)=> handleChange(e)}
-        value={form.lastName}
-      />
-      <input
-        className="border-2 border-black p-5"
-        placeholder="Email"
-        type="email"
-        name="email"
-        onChange={(e)=> handleChange(e)}
-        value={form.email}
-      />
-      <input
-        className="border-2 border-black p-5"
-        placeholder="Password"
-        type="password"
-        name="password"
-        onChange={(e)=> handleChange(e)}
-        value={form.password}
-      />
-      <input
-        className="border-2 border-black p-5"
-        placeholder="Confirm Password"
-        type="password"
-        name="confirmPassword"
-        onChange={(e)=> handleChange(e)}
-        value={form.confirmPassword}
-      />
-      <input
-        className="border-2 border-black p-5"
-        placeholder="Phone"
-        type="tel"
-        name="phone"
-        onChange={(e)=> handleChange(e)}
-        value={form.phone}
-      />
-      <button>Submit</button>
-    </form>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
