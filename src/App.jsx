958f760869ce8d0b553be32dcbb46e0781094e0a
Nusrat1002 <nusrat.j.jannat103@gmail.com>
2026-05-10 22:41:14 +0600

    applied axios to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { useState, Fragment, useEffect } from "react";
+import axios from "axios";
 
-export default App
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const fetchUsers = async () => {
+    const response = await axios.get("https://dummyjson.com/users");
+    setUsers(response.data.users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <Fragment key={user.id}>
+          <div>
+            <h1>{user.firstName}</h1>
+            <h1>{user.lastName}</h1>
+            <h1>{user.email}</h1>
+          </div>
+          <hr />
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default App;
