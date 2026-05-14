a6800e0b1215479b5724b388a3e1b6599f1d7f48
Nusrat1002 <nusrat.j.jannat103@gmail.com>
2026-05-15 00:35:41 +0600

    created custom hook named useFetch.js

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import api from "../apiConfig";
+
+
+export const useFetch = ( endpoint )=>{
+    const [data, setData ] = useState([]);
+    const [loading. setLoading ] = useState(true);
+    const [errors, setErrors ] = useState(true);
+
+    useEffect(()=> {
+        const fetchApi = async () => {
+          try{
+            const response = await api.get(endpoint, {
+                ...options,
+            });
+            setData(response.data);
+        } catch(error){
+            setErrors(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    fetchApi();
+    }, [endpoint, options]);
+    return {
+        data, loading, errors
+    };
+};
